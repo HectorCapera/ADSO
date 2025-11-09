2c17cd91cbeb319b873f879455c19cf2a3686ad2
alexanderg349 <alexandergomezgiraldo@gmail.com>
2025-11-09 11:33:49 -0500

    Setup inicial de Spring Boot

---
+++ citas-psicologia-app/citas-psicologia-app/src/main/java/com/sena/citaspsicologia/citas_psicologia_app/CitasPsicologiaAppApplication.java
@@ -0,0 +1,13 @@
+package com.sena.citaspsicologia.citas_psicologia_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitasPsicologiaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitasPsicologiaAppApplication.class, args);
+	}
+
+}
