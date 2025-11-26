f9df87a0ddced4cb53335c323ae59f6ab60a45a1
alexanderg349 <alexandergomezgiraldo@gmail.com>
2025-11-26 01:12:35 -0500

    Frontend funcional: modulo de citas terminado

--- frontend-citas/src/services/CitaService.js
+++ frontend-citas/src/services/CitaService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/citas";
+const API_URL = "http://localhost:8090/api/citas";
 
 export const listarCitas = async () => {
     return await axios.get(API_URL);
